d7263a9ca98c063b57cda7eac72df37ac506927d
Mynul Islam <mynul.sakil@gmail.com>
2024-08-12 00:00:35 +0600

    finish coparation with frontend and backend

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
+
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
+
+  const login = async (username, password) => {
+    setLoading(true);
+    const success = handleInputError(username, password);
+    if (!success) return;
+    try {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await res.json();
+      if (data.error) throw new Error(data.error);
+      //   set local storage
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      // set context
+      setAuthUser(data);
+    } catch (err) {
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { loading, login };
+};
+
+export default useLogin;
+function handleInputError(username, password) {
+  if (!username || !password) {
+    toast.error("Please fill in all fields");
+    return false;
+  }
+
+  if (password.length < 6) {
+    toast.error("Password must be at least 6 characters long");
+    return false;
+  }
+  return true;
+}
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
 import { useState } from "react";
+import useSignup from "../../hooks/useSignup";
 
 const SignUp = () => {
   const [inputs, setInputs] = useState({
@@ -10,14 +11,14 @@ const SignUp = () => {
     confirmPassword: "",
     gender: "",
   });
-
+  const { signup, loading } = useSignup();
   const handleCheckboxChange = (gender) => {
     setInputs({ ...inputs, gender });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(inputs);
+    await signup(inputs);
   };
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
@@ -87,7 +88,10 @@ const SignUp = () => {
             />
           </div>
 
-          <GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} />
+          <GenderCheckbox
+            onCheckboxChange={handleCheckboxChange}
+            selectedGender={inputs.gender}
+          />
 
           <Link
             className="text-sm text-gray-100 hover:underline hover:text-blue-200 mt-2 inline-block"
@@ -97,8 +101,15 @@ const SignUp = () => {
           </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              Sign Up
+            <button
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
         </form>
