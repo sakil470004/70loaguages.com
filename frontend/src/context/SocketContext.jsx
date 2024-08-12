a6453be4d9a7971a5d9e6ffe022fa9aa224ee467
Mynul Islam <mynul.sakil@gmail.com>
2024-08-13 01:48:52 +0600

    change back for working

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://seven0loaguages-com.onrender.com", {
+			const socket = io("http://localhost:5000", {
 				query: {
 					userId: authUser._id,
 				},
