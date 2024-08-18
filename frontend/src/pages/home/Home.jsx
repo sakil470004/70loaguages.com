1f1545e1903d948226ef72a8914208d00bc45144
Mynul Islam <mynul.sakil@gmail.com>
2024-08-19 00:32:42 +0600

    added navbar

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,10 @@
-
-
-
+import Navbar from "../../components/navbar/Navbar";
 
 const Home = () => {
-	return (
-		<div>
-			his
-		</div>
-	);
+  return (
+    <div className="container mx-auto ">
+      <Navbar />
+    </div>
+  );
 };
 export default Home;
