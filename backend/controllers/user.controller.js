c4ee783d749271cb6b0ee9c6deddab131f34565b
Mynul Islam <mynul.sakil@gmail.com>
2024-08-30 00:34:55 +0600

    adding refer api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -19,17 +19,51 @@ export const getUsersForSidebar = async (req, res) => {
 export const makeAdmin = async (req, res) => {
   try {
     const { userId } = req.body;
-    const user = await User.findOne({_id : userId});
+    const user = await User.findOne({ _id: userId });
 
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
     user.admin = true;
     await user.save();
-    
+
     res.status(200).json({ message: "User is now an admin" });
   } catch (error) {
     console.log("Error in makeAdmin controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+export const referUser = async (req, res) => {
+  try {
+    const { newUserId, commissionsUserId } = req.body;
+    const commissionsUser = await User.findOne({ _id: commissionsUserId });
+    if (!commissionsUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const isAlreadyReferred = commissionsUser.commissions.includes(newUserId);
+    if (isAlreadyReferred) {
+      return res.status(400).json({ message: "User already referred" });
+    }
+    // get newUser by newUserId
+    const newUser = await User.findOne({ _id: newUserId });
+    if (!newUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    // check if user is already referred
+    if (newUser.referredBy) {
+      return res.status(400).json({ message: "User already referred" });
+    }
+    // update commissions array . push newUserId to commissions array
+    commissionsUser.commissions.push(newUserId);
+    await commissionsUser.save();
+
+    // add referredBy filed using commissionsUserId
+    newUser.referredBy = commissionsUserId;
+    await newUser.save();
+
+    res.status(200).json({ message: "User referred successfully" });
+  } catch (error) {
+    console.log("Error in user controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUsersForSidebar,makeAdmin } from "../controllers/user.controller.js";
+import { getUsersForSidebar,makeAdmin,referUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
-
+// this route is used to make a user an admin
 router.post("/makeadmin/",protectRoute, makeAdmin);
+// this route is used to get all users for the sidebar
 router.get("/",protectRoute, getUsersForSidebar);
+// this route used to refer a user to another user
+router.post("/refer/",protectRoute, referUser);
 
 
 export default router;
