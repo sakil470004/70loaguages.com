8cbfc89a9353320fcb10e1234b53bf1f28a8eef8
Mynul Islam <mynul.sakil@gmail.com>
2024-09-04 23:47:15 +0600

    fix few things

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,9 +1,21 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import cors from "cors";
+
 
 const app = express();
-
+app.use(
+  cors({
+    origin: "*", // This allows requests from any origin
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    exposedHeaders: ["Content-Type", "Authorization"], // Exposed headers
+    credentials: true, // Allow credentials (cookies, HTTP authentication)
+    preflightContinue: false, // Pass the CORS preflight response to the next handler
+    optionsSuccessStatus: 204, // Status code for successful OPTIONS request
+  })
+);
 
 const server = http.createServer(app);
 // create new server based on port of the frontend server
