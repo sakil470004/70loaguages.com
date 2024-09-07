a22d154c91bcd361e38760f442b72b3d2d4e8649
Mynul Islam <mynul.sakil@gmail.com>
2024-09-08 04:10:55 +0600

    for my aws link

--- frontend/APP_URL.js
+++ frontend/APP_URL.js
@@ -1,3 +1,3 @@
-const APP_URL ='http://3.25.92.116';
-// const APP_URL ='http://localhost:5000';
+// const APP_URL ='http://3.25.92.116';
+const APP_URL ='http://localhost:5000';
 export default APP_URL
