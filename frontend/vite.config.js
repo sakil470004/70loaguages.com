3788e71939cb5fdd970bf939e2307329e9803035
Mynul Islam <mynul.sakil@gmail.com>
2024-09-08 04:05:53 +0600

    adding extra stff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import APP_URL from "./APP_URL";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: `${APP_URL}`, // Your backend server (local or production)
+        changeOrigin: true,  // Ensure the host header is changed to the target
+        rewrite: (path) => path.replace(/^\/api/, ""), // Optionally remove '/api' prefix from the path
+        secure: false,  // Set to true if your backend is served over HTTPS
+      },
+    },
+  },
+});
