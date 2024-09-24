53db97472ccd7812a20a4c8bec20a2c7fc26d955
Mynul Islam <mynul.sakil@gmail.com>
2024-09-25 05:35:48 +0600

    adding updated frontend

--- frontend/APP_URL.js
+++ frontend/APP_URL.js
@@ -1,11 +1,11 @@
 //example:  mynul url
 // const APP_URL ='http://3.25.92.116';
 // elastic server
-// const APP_URL ='http://3.104.219.74';
+const APP_URL ='http://3.104.219.74';
 //example: danial server; 
 // const APP_URL ='http://13.50.147.100';
 //example: for internal server
-const APP_URL ='http://localhost:80';
+// const APP_URL ='http://localhost:80';
 // for local server
 // const APP_URL ='http://localhost:5000';
 export default APP_URL
