bca981505d3b0d01d51b91ad08b12f2bd020642c
Mynul Islam <mynul.sakil@gmail.com>
2024-09-29 01:12:58 +0600

    pushing build file

--- frontend/APP_URL.js
+++ frontend/APP_URL.js
@@ -1,11 +1,11 @@
 //example:  mynul url
-// const APP_URL ='http://3.25.92.116';
-// elastic server
-// const APP_URL ='http://3.104.219.74';
+// it is not ilastic ip of server
+const APP_URL ='http://3.25.148.210';
 //example: danial server; 
+// this is elastic ip of server
 // const APP_URL ='http://13.50.147.100';
 //example: for internal server
 // const APP_URL ='http://localhost:80';
 // for local server
-const APP_URL ='http://localhost:5000';
+// const APP_URL ='http://localhost:5000';
 export default APP_URL
