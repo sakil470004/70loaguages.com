ccc45044c1be59568a07f17c32d428760b27805c
Mynul Islam <mynul.sakil@gmail.com>
2024-11-26 22:45:50 +0600

    for aws

--- frontend/APP_URL.js
+++ frontend/APP_URL.js
@@ -5,7 +5,7 @@
 // this is elastic ip of server
 // const APP_URL ='http://13.50.147.100';
 //example: for internal server
-// const APP_URL ='http://localhost:80';
+const APP_URL ='http://localhost:80';
 // for local server
-const APP_URL ='http://localhost:5000';
+// const APP_URL ='http://localhost:5000';
 export default APP_URL
